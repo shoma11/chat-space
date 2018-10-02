234c1d5b18e3c27e9d2b085b1cfd8a821d9637f1
菊川政真 <sk1107.eg@gmail.com>
2018-10-02 21:30:02 +0900

    change_groups_and_members_table

--- db/migrate/20181002110004_create_members.rb
+++ db/migrate/20181002110004_create_members.rb
@@ -3,7 +3,6 @@ class CreateMembers < ActiveRecord::Migration[5.0]
     create_table :members do |t|
       t.references :user,  null: false, foreign_key: true
       t.references :group, null: false, foreign_key: true
-
       t.timestamps
     end
   end
