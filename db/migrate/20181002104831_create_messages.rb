7c1babaaa40bef336a9abca6b4567f1f35ec5d21
菊川政真 <sk1107.eg@gmail.com>
2018-10-13 16:35:28 +0900

    データベース設計変更

--- db/migrate/20181002104831_create_messages.rb
+++ db/migrate/20181002104831_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text        :body
+      t.text        :content
       t.string      :image
-      t.references  :group,  null: false,  foreign_key: true
-      t.references  :user,   null: false,  foreign_key: true
+      t.references  :group, foreign_key: true
+      t.references  :user,  foreign_key: true
       t.timestamps
     end
   end
