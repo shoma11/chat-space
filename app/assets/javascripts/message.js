42ca3194b9975b8cf14acbae1f2189b2998c5d98
菊川政真 <sk1107.eg@gmail.com>
2018-10-18 18:16:05 +0900

    HTMLを作成するメソッド の定義

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,22 @@
 $(function(){
+  // HTMLを作成するメソッド
+  function buildHTML(message){
+    var image_url = (message.image_url)? `<image class="message_image" src="${message.image_url}">`:"";
+    var html = `<div class="message" id='${message.id}'>
+                  <div class="message__user-name">
+                  ${message.name}
+                  </div>
+                  <div class="message__date">
+                  ${message.date}
+                  </div>
+                  <div class="message__content">
+                  ${message.content}
+                  ${image_url}
+                  </div>
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.content @message.content
 json.name @message.user.name
-json.time @message.created_at.to_s
+json.date @message.created_at.to_s
 json.image_url @message.image_url
 json.id @message.id
